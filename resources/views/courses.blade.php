5a32d7a84f7a1ec97db6764961e26e9b8d353407
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-03-24 17:01:39 +0600

    break

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('main-section')
+
+<h1 class="text-center">
+    Courses Page
+</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{name?}',function($name = null){
-    $demo = "<h2>abc</h2>";
-    $data = compact('name','demo');
-    return view('home')->with($data);
+Route::get('/',function(){
+    return view('home');
+});
+
+Route::get('/about',function(){
+    return view('about');
+});
+
+Route::get('/course',function(){
+    return view('courses');
 });
