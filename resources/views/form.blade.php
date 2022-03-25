b80bc9df7a98fca0afa6e418a18419b31a5e64f5
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-03-25 17:14:28 +0600

    close to noon

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -13,25 +13,44 @@
   <body>
       <form action="{{url('/')}}/register" method="post">
         @csrf
-        <pre>
-          @php
-              print_r($errors->all());
-          @endphp
-        </pre>
       <div class="container">
           <h1 class="text-center">Registration</h1>
           <div class="form-group">
               <label for="">Name</label>
               <input type="text" name="name" id="" class="form-control" placeholder="" aria-description"helpId">
               <!-- <small id="helpId" class="text-muted">Help text</small> -->
+              <span class="text-danger">
+              @error('name')
+                    {{$message}}
+              @enderror
+              </span>
 </div><div class="form-group">
               <label for="">Email</label>
               <input type="email" name="email" id="" class="form-control" placeholder="" aria-description"helpId">
               <!-- <small id="helpId" class="text-muted">Help text</small> -->
-</div><div class="form-group">
+              <span class="text-danger">
+              @error('email')
+              {{$message}}
+              @enderror
+              </span>
+              </div><div class="form-group">
               <label for="">Password</label>
               <input type="password" name="password" id="" class="form-control" placeholder="" aria-description"helpId">
               <!-- <small id="helpId" class="text-muted">Help text</small> -->
+              <span class="text-danger">
+              @error('password')
+              {{$message}}
+              @enderror
+              </span>
+</div><div class="form-group">
+              <label for="">Confirm Password</label>
+              <input type="password" name="confirm_password" id="" class="form-control" placeholder="" aria-description"helpId">
+              <!-- <small id="helpId" class="text-muted">Help text</small> -->
+              <span class="text-danger">
+              @error('confirm_password')
+                  {{$message}}
+              @enderror
+              </span>
 </div>
 <button class="btn btn-primary">
     Submit
