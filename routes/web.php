621db0be0731d25b97a3949d68812387baa473ee
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-03-25 09:14:07 +0600

    early one

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\DemoController;
+use App\Http\Controllers\DemoController;
 
 /*
 |--------------------------------------------------------------------------
