45a74bf44ba3589a6c7f814fb3a931a19e7a29d2
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-03-26 18:50:45 +0600

    noon

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+              <label for="">Name</label>
+              <input type="text" name="name" id="" class="form-control" value="{{old('name')}}" placeholder="" aria-description"helpId">
+              <!-- <small id="helpId" class="text-muted">Help text</small> -->
+              <span class="text-danger">
+              <!-- @error('name')
+                    {{$message}}
+              @enderror -->
+              </span>
+</div>
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -24,33 +24,6 @@
                     {{$message}}
               @enderror
               </span>
-</div><div class="form-group">
-              <label for="">Email</label>
-              <input type="email" name="email" id="" class="form-control" value="{{old('email')}}" placeholder="" aria-description"helpId">
-              <!-- <small id="helpId" class="text-muted">Help text</small> -->
-              <span class="text-danger">
-              @error('email')
-              {{$message}}
-              @enderror
-              </span>
-              </div><div class="form-group">
-              <label for="">Password</label>
-              <input type="password" name="password" id="" class="form-control" placeholder="" aria-description"helpId">
-              <!-- <small id="helpId" class="text-muted">Help text</small> -->
-              <span class="text-danger">
-              @error('password')
-              {{$message}}
-              @enderror
-              </span>
-</div><div class="form-group">
-              <label for="">Confirm Password</label>
-              <input type="password" name="password_confirmation" id="" class="form-control" placeholder="" aria-description"helpId">
-              <!-- <small id="helpId" class="text-muted">Help text</small> -->
-              <span class="text-danger">
-              @error('password_confirmation')
-                  {{$message}}
-              @enderror
-              </span>
 </div>
 <button class="btn btn-primary">
     Submit
